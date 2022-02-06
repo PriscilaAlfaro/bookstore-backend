12b6c2c26beae9ed06b2040abd8427a9203de7a9
Priscila Alfaro <priscila24n@hotmail.com>
2022-02-06 14:37:05 +0100

    add endpoint for salesOrder

--- controllers/salesOrders.js
+++ controllers/salesOrders.js
@@ -1,6 +1,8 @@
 /* eslint-disable camelcase */
 const express = require('express');
 const SalesOrders = require('../models/salesOrders');
+const { Buffer } = require('buffer');
+const fetch = require('node-fetch');
 
 const salesOrderRouter = express.Router();
 
@@ -106,6 +108,66 @@ salesOrderRouter.post('/', async (req, res) => {
             res.status(200).json({ salesOrder: savedSalesOrder, success: "true" });
         } else {
             return res.status(400).json({ message: "Bad request", success: "true" });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+})
+
+salesOrderRouter.post('/checkout/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        if (userId) {
+            const body = {
+                purchase_country: "SE",
+                purchase_currency: "SEK",
+                locale: "en-se",
+                order_amount: 50000,
+                order_tax_amount: 4545,
+                order_lines: [
+                    {
+                        type: "physical",
+                        reference: "19-402-USA",
+                        name: "Red T-Shirt",
+                        quantity: 5,
+                        quantity_unit: "pcs",
+                        unit_price: 10000,
+                        tax_rate: 1000,
+                        total_amount: 50000,
+                        total_discount_amount: 0,
+                        total_tax_amount: 4545
+                    }
+                ],
+                merchant_urls: {
+                    terms: "https://www.example.com/terms.html",
+                    checkout: "https://www.example.com/checkout.html",
+                    confirmation: "https://www.example.com/confirmation.html",
+                    push: "https://www.example.com/api/push"
+                }
+            }
+
+            const base64Secrets = Buffer.from(process.env.KARNA_USERNAME + ':' + process.env.KLARNA_PASSWORD).toString('base64');
+
+            const options = {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: "Basic " + base64Secrets,
+                }
+
+            }
+
+            const url = "https://api.playground.klarna.com/checkout/v3/orders"
+
+            return fetch(url, options)
+                .then(res => res.json())
+                .then(klarnaResponse =>
+                    res.status(200).json({ response: klarnaResponse, success: true }));
+
+        } else {
+            return res.status(400).json({ message: "Bad request", success: false });
         }
     } catch (error) {
         return res.status(500).json({ message: error.message });
