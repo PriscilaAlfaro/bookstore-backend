cbb004c1f6346d7ccd735959961e0b95703b3666
Priscila Alfaro <priscila24n@hotmail.com>
2022-02-06 17:52:57 +0100

    add logic for salesOrder inKlarna

--- controllers/salesOrders.js
+++ controllers/salesOrders.js
@@ -1,6 +1,10 @@
 /* eslint-disable camelcase */
 const express = require('express');
+const mongoose = require('mongoose');
 const SalesOrders = require('../models/salesOrders');
+const Carts = require('../models/carts');
+const Books = require('../models/books');
+
 const { Buffer } = require('buffer');
 const fetch = require('node-fetch');
 
@@ -110,7 +114,7 @@ salesOrderRouter.post('/', async (req, res) => {
             return res.status(400).json({ message: "Bad request", success: "true" });
         }
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.response });
     }
 })
 
@@ -118,59 +122,96 @@ salesOrderRouter.post('/checkout/:userId', async (req, res) => {
     try {
         const { userId } = req.params;
 
+
         if (userId) {
-            const body = {
-                purchase_country: "SE",
-                purchase_currency: "SEK",
-                locale: "en-se",
-                order_amount: 50000,
-                order_tax_amount: 4545,
-                order_lines: [
-                    {
+            const userIdAsObjectId = mongoose.Types.ObjectId(userId);
+            const cartAlreadyExists = await Carts.exists({ userId: userIdAsObjectId });
+
+            if (cartAlreadyExists) {
+                //hacer el fetch a klarna
+                //completar los datos con lo que hay en el backend
+                const cart = await Carts.find({ userId: userIdAsObjectId });
+                const productIdBatch = cart[0]?.items.map(item => item.productId);
+                const books = await Books.find({ _id: { $in: productIdBatch } });
+
+
+                //Klarna checkout
+                const taxRate = 0.13;
+
+                const itemsInfo = books.map(book => {
+                    const cartItem = cart[0].items.find(item => item.productId.equals(book._id))
+
+                    const unitPrice = book.price * 100;  //book.price
+                    const unitTaxAmount = unitPrice * taxRate;
+                    const unitPriceWithTax = unitPrice + unitTaxAmount;
+
+                    const totalAmountWitTaxes = unitPriceWithTax * cartItem.quantity;
+                    const totalTaxAmount = (unitPrice * taxRate) * cartItem.quantity;
+
+
+                    return {
                         type: "physical",
-                        reference: "19-402-USA",
-                        name: "Red T-Shirt",
-                        quantity: 5,
+                        reference: book._id,
+                        name: book.title,
+                        quantity: cartItem.quantity, //book.quantity
                         quantity_unit: "pcs",
-                        unit_price: 10000,
-                        tax_rate: 1000,
-                        total_amount: 50000,
+                        unit_price: unitPriceWithTax, //unitPrice + unitTaxAmount, //Includes tax. Example: 100 Euros should be 10000.
+                        tax_rate: taxRate * 10000, //The percentage value is represented with two implicit decimals. Example: 25 % should be 2500.
+                        total_amount: totalAmountWitTaxes, //Includes tax. Example: 25 euros should be 2500 Value = (quantity x unit_price)
                         total_discount_amount: 0,
-                        total_tax_amount: 4545
+                        total_tax_amount: totalTaxAmount //Must be within ±1 of total_amount - total_amount * 10000 / (10000 + tax_rate). Negative when type is discount.
                     }
-                ],
-                merchant_urls: {
-                    terms: "https://www.example.com/terms.html",
-                    checkout: "https://www.example.com/checkout.html",
-                    confirmation: "https://www.example.com/confirmation.html",
-                    push: "https://www.example.com/api/push"
+                });
+
+                console.log("itemsInfo", itemsInfo)
+                const orderAmount = itemsInfo.reduce((a, b) => a + b.total_amount, 0);
+                const orderTaxAmount = itemsInfo.reduce((a, b) => a + b.total_tax_amount, 0);
+
+                const body = {
+                    purchase_country: "SE",
+                    purchase_currency: "SEK",
+                    locale: "en-se",
+                    order_amount: orderAmount, //Total amount of the order including tax and any available discounts.
+                    order_tax_amount: orderTaxAmount, //Total TAX amount of the order. The value should be in non-negative minor units.
+                    order_lines: itemsInfo,
+                    merchant_urls: {
+                        terms: "https://www.example.com/terms.html",
+                        checkout: "http://localhost:3000/payment",
+                        confirmation: "https://www.example.com/confirmation.html",
+                        push: "https://www.example.com/api/push"
+                    }
                 }
-            }
 
-            const base64Secrets = Buffer.from(process.env.KARNA_USERNAME + ':' + process.env.KLARNA_PASSWORD).toString('base64');
+                const base64Secrets = Buffer.from(process.env.KARNA_USERNAME + ':' + process.env.KLARNA_PASSWORD).toString('base64');
 
-            const options = {
-                method: 'POST',
-                body: JSON.stringify(body),
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: "Basic " + base64Secrets,
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(body),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: "Basic " + base64Secrets,
+                    }
+
                 }
 
+                const url = "https://api.playground.klarna.com/checkout/v3/orders"
+
+                return fetch(url, options)
+                    .then(res => res.json())
+                    .then(klarnaResponse =>
+                        res.status(200).json({ response: klarnaResponse, success: true }));
+
+
+
+            } else {
+                return res.status(404).json({ response: "cart not found", success: false });
             }
-
-            const url = "https://api.playground.klarna.com/checkout/v3/orders"
-
-            return fetch(url, options)
-                .then(res => res.json())
-                .then(klarnaResponse =>
-                    res.status(200).json({ response: klarnaResponse, success: true }));
 
         } else {
-            return res.status(400).json({ message: "Bad request", success: false });
+            return res.status(400).json({ response: "Bad request", success: false });
         }
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({ response: error.message });
     }
 })
 
